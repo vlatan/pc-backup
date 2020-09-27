f87d36d108fb1d29c0d8d56a507ec6bc37895610
vlatan <33163699+vlatan@users.noreply.github.com>
2020-09-27 17:55:29 +0200

    Move function to helpers.py

--- pc-backup-sdk.py
+++ pc-backup-sdk.py
@@ -6,32 +6,6 @@ from variables import *
 from helpers import *
 
 
-def compute_diff(new_index, old_index, bucket):
-    """ Computes the differences between the bucket, the
-        new index and the old index.
-        new_index: newly computed directory index (dict).
-        old_index: old directory index from a json file (dict).
-        bucket: S3 bucket boto3 instance.
-        Returns: Dictionary of deleted/created/modified files. """
-
-    data = {}
-    # get the bucket files
-    bucket_files = set(f.key for f in bucket.objects.all())
-    # get keys from indexes, which are in fact the files
-    new_index_files = set(new_index.keys())
-    old_index_files = set(old_index.keys())
-    # files present in the S3 bucket but not in the new index
-    data['deleted'] = list(bucket_files - new_index_files)
-    # files present in the new index but not in the S3 bucket
-    data['created'] = list(new_index_files - bucket_files)
-    # files present both in the new index and the old index
-    # but with different last modified times
-    common_files = old_index_files.intersection(new_index_files)
-    data['modified'] = [f for f in common_files if new_index[f] != old_index[f]]
-
-    return data
-
-
 def bulk_delete_files(files, bucket):
     # remove objects from S3 bucket
     bucket.delete_objects(
