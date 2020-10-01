eff055fc1f544cc2f0b6eab05ee9c54f03f590c2
vlatan <33163699+vlatan@users.noreply.github.com>
2020-10-02 01:23:41 +0200

    Thread with submit instead of map, check completed jobs

--- pc-backup-sdk.py
+++ pc-backup-sdk.py
@@ -1,9 +1,7 @@
 #! /usr/bin/env python
 
-import logging
 import boto3
-from botocore.exceptions import ClientError
-from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import ThreadPoolExecutor, as_completed
 from variables import *
 from helpers import *
 
@@ -16,21 +14,19 @@ def handle_object(args):
         key: an object key
         filename: local path to file
         storage_class: the storage class of the object
-        delete: True -> delete object, False -> upload file
-        return: None. """
+        delete: if True delete object, if False upload file
+        return: True """
     client, bucket_name, key = args[0], args[1], args[2]
     filename, storage_class, delete = args[3], args[4], args[5]
-    try:
-        if delete:
-            client.delete_object(Bucket=bucket_name,
-                                 Key=key)
-        else:
-            client.upload_file(Filename=filename,
-                               Bucket=bucket_name,
-                               Key=key,
-                               ExtraArgs={'StorageClass': storage_class})
-    except ClientError as e:
-        logging.error(e)
+    if delete:
+        client.delete_object(Bucket=bucket_name,
+                             Key=key)
+    else:
+        client.upload_file(Filename=filename,
+                           Bucket=bucket_name,
+                           Key=key,
+                           ExtraArgs={'StorageClass': storage_class})
+    return True
 
 
 if __name__ == '__main__':
@@ -45,19 +41,20 @@ if __name__ == '__main__':
     # if there's a difference in the indexes (old and new)
     if new_index != old_index:
 
-        # configure logging, log_file is defined in variables.py
-        logging.basicConfig(filename=log_file, level=logging.DEBUG)
-
-        # create S3 resource
+        # create an S3 resource
         s3 = boto3.resource('s3')
-        # create an instance of the bucket
+        # create an instance of the S3 bucket object
         bucket = s3.Bucket(bucket_name)
 
         # which objects to delete/upload
         data = compute_diff(new_index, old_index, bucket)
 
+        # instantiate an S3 client object
         client = boto3.client('s3')
 
+        # construct a list of lists filled with parameters for every S3 key
+        # so we can later easily map the parameters
+        # to the function that handles objects
         args = []
         for key in data['deleted']:
             args.append([client, bucket_name, key, None, None, True])
@@ -65,14 +62,25 @@ if __name__ == '__main__':
             args.append([client, bucket_name, key,
                          f'{user_root}/{key}', 'STANDARD_IA', False])
 
-        # delete/upload files in parallel
-        with ThreadPoolExecutor(max_workers=len(args)) as executor:
-            executor.map(handle_object, args)
+        # delete/upload files concurrently
+        with ThreadPoolExecutor(max_workers=len(args) + 10) as executor:
+            future_keys = {executor.submit(handle_object, args[i]):
+                           [args[i][2], args[i][5]] for i in range(len(args))}
 
-        num_deleted = len(data['deleted'])
-        num_uploaded = len(data['created'] or data['modified'])
+            deleted, uploaded, failed = 0, 0, 0
+            for future in as_completed(future_keys):
+                key, job = future_keys[future][0], future_keys[future][1]
+                try:
+                    output = future.result()
+                    if job:
+                        deleted += 1
+                    else:
+                        uploaded += 1
+                except Exception as e:
+                    failed += 1
+                    print(f'{key} generated an exception {e}')
 
-        logging.info(f'Deleted: {num_deleted}. Uploaded: {num_uploaded}.')
+        print(f'Deleted: {deleted}. Uploaded: {uploaded}. Failed: {failed}')
 
         # save/overwrite the json index file
         save_json(json_index_file, new_index)
