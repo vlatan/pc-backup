4e46918dcc9893b80c68455735da6b581132fb3f
vlatan <33163699+vlatan@users.noreply.github.com>
2024-07-26 16:57:01 +0200

    Check bucket on every call

--- sdk.py
+++ sdk.py
@@ -1,9 +1,9 @@
-import sys
 import json
 import boto3
 import psutil
 import asyncio
 import logging
+import hashlib
 import botocore.config
 from pathlib import Path
 from utils import init_set_up
@@ -31,53 +31,36 @@ CLIENT = s3.meta.client  # instantiate an S3 low-level client (thread safe)
 
 
 async def main() -> None:
-    """
-    If there's a change in the directories
-    delete/upload files from/to s3 bucket.
-    """
+    """Sync directories with s3 bucket."""
     # perform initial setup
     init_set_up()
 
-    # get the old index
-    index_path = Path("logs") / "index.json"
-    old_index = json.loads(index_path.read_text()) if index_path.exists() else {}
-
-    # get the new index
-    new_index = await compute_index()
-
-    # if no change in index exit
-    if new_index == old_index:
-        sys.exit()
-
-    # determine which objects to delete/upload
-    data = compute_diff(new_index, old_index)
+    # compute local file index
+    index = await compute_index()
 
     # aynchronysly update S3 bucket (delete/update/upload files)
-    await update_bucket(data)
-
-    # save/overwrite the json index file with the fresh new index
-    index_path.write_text(json.dumps(new_index, indent=4))
+    await update_bucket(index)
 
 
-async def compute_index() -> dict[str, float]:
+async def compute_index() -> set[str]:
     """Compute index on every dict concurrently."""
 
     # execute coroutines concurrently and await for all results to come
     coros = [asyncio.to_thread(compute_dir_index, Path(d)) for d in DIRECTORIES]
     async with asyncio.TaskGroup() as tg:
-        indexes = [tg.create_task(coro) for coro in coros]
+        tasks = [tg.create_task(coro) for coro in coros]
 
-    # merge all dicts into one index
-    return {key: value for index in indexes for key, value in index.result().items()}
+    # merge all sets into one
+    return {key for index in tasks for key in index.result()}
 
 
-def compute_dir_index(root_dir: Path) -> dict[str, float]:
+def compute_dir_index(root_dir: Path) -> set[str]:
     """
-    Computes a directory's index of files and their last modified times.
+    Computes a directory's index of files.
     root_dir: absolute path to the root directory
-    Return: dictionary with files absolute paths and their last modified time
+    Return: set with files absolute paths
     """
-    index = {}
+    index = set()
     # exclusion helper function
     permitted = lambda x: not (x.startswith(PREFIXES) or x.endswith(SUFFIXES))
     # traverse the path recursively
@@ -88,55 +71,46 @@ def compute_dir_index(root_dir: Path) -> dict[str, float]:
         file_names[:] = [f for f in file_names if permitted(f)]
         # loop through the file names in the current directory
         for file_name in file_names:
-            # get the file's absolute path
-            file_path = current_root / file_name
-            if mtime := file_path.stat().st_mtime if file_path.exists() else None:
-                # record the file's last modification time if exists
-                index[str(file_path)] = mtime
+            # add the file's absolute path to set
+            if (filepath := current_root / file_name).exists():
+                index.add(str(filepath))
     return index
 
 
-def compute_diff(
-    new_index: dict[str, float], old_index: dict[str, float]
-) -> dict[str, list[str]]:
-    """
-    Computes the differences between the S3 bucket, new index and old index.
-    new_index: newly computed directory index (dict)
-    old_index: old directory index from a json file (dict)
-    Return: dictionary of deleted/created/modified files
-    """
-    # get keys/files from indexes and the bucket
-    new_index_files = set(new_index.keys())
-    old_index_files = set(old_index.keys())
-    bucket_files = set(f.key for f in BUCKET.objects.all())
-
-    data = {}
-    # files in the S3 bucket but not in the new index (deleted files) - sets difference
-    data["deleted"] = list(bucket_files - new_index_files)
-    # files in the new index but not in the S3 bucket (new files) - sets diference
-    data["created"] = list(new_index_files - bucket_files)
-    # files both in the old index and the new index (common files) - sets intersection
-    common_files = old_index_files & new_index_files
-    # common files with different last modified times (modified files)
-    data["modified"] = [f for f in common_files if new_index[f] != old_index[f]]
-
-    return data
-
-
-async def update_bucket(data: dict[str, list[str]]) -> None:
+async def update_bucket(index: set[str]) -> None:
     """
     Create the needed S3 resources and instances and
     delete/upload files concurrently.
     data: dictionary of deleted, new and modified files
     Return: None
     """
+
+    # get all bucket files
+    all_bucket_objects = BUCKET.objects.all()
+    bucket_files = set(f"/{f.key}" for f in all_bucket_objects)
+
     # files can be deleted in batches of max 1000 files per batch
-    to_delete = [{"Key": key.lstrip("/")} for key in data.get("deleted", [])]
+    to_delete = [{"Key": key.lstrip("/")} for key in (bucket_files - index)]
     to_delete = [to_delete[i : i + 1000] for i in range(0, len(to_delete), 1000)]
     coros = [asyncio.to_thread(bulk_delete_s3_objects, lst) for lst in to_delete]
 
-    # sort the upload files by size
-    to_upload = data.get("created", []) + data.get("modified", [])
+    # which files to uplaod
+    to_upload = list(index - bucket_files)
+    for obj in all_bucket_objects:
+        if (key := f"/{obj.key}") not in index:
+            continue
+
+        if not (filepath := Path(key)).exists():
+            continue
+
+        if obj.size != filepath.stat().st_size:
+            to_upload.append(key)
+            continue
+
+        if obj.e_tag.strip('"') != e_tag(filepath):
+            to_upload.append(key)
+
+    # sort upload files by size
     to_upload.sort(key=lambda f: Path(f).stat().st_size)
     coros += [asyncio.to_thread(upload_s3_object, key) for key in to_upload]
 
@@ -174,6 +148,11 @@ def upload_s3_object(key: str) -> None:
         )
     except (ClientError, BotoCoreError) as e:
         logging.error(e)
+
+
+def e_tag(filepath: Path) -> str:
+    with open(filepath, "rb") as f:
+        return hashlib.file_digest(f, "md5").hexdigest()
 
 
 if __name__ == "__main__":
